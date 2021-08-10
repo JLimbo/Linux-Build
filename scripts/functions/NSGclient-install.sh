b886598c082e987ce5cc6af3c2b71e5970d1935a
john <john.limb@iress.com>
2021-08-10 11:57:05 +0100

    Update NSGclient-install.sh

--- scripts/functions/NSGclient-install.sh
+++ scripts/functions/NSGclient-install.sh
@@ -23,5 +23,6 @@ printf "    Downloading prerequesites for NSG client    \\n"
 printf "\\n"
 wget http://ftp.br.debian.org/debian/pool/main/n/network-manager/libnm-util2_1.6.2-3+deb9u2_amd64.deb http://ftp.br.debian.org/debian/pool/main/n/network-manager/libnm-glib4_1.6.2-3+deb9u2_amd64.deb
 sudo apt install ./libnm-util2_1.6.2-3+deb9u2_amd64.deb ./libnm-glib4_1.6.2-3+deb9u2_amd64.deb 
-
 sudo apt install libappindicator1 libproxy1-plugin-webkit libnl-cli-3-200
+printf "\\n"
+printf "    You should now be able to install NSG client 18.04 on new versions of linux\\n"
